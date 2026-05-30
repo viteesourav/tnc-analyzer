a608b7343e7f443910a0561df423f5d6d26f2768
sourav <souravm.mitra@gmail.com>
2026-05-30 18:38:47 +0530

    fix: postman collection added and application startup notes added in work-note

--- analysis-service/src/main/java/com/tnc/controller/AnalysisServiceController.java
+++ analysis-service/src/main/java/com/tnc/controller/AnalysisServiceController.java
@@ -1,7 +1,5 @@
 package com.tnc.controller;
 
-import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
